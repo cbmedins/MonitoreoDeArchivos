cd6454300ad10639aa071f9948c99bd5eaffd922
Cristhian Medina <cbmedins@gmail.com>
2021-02-25 02:38:01 -0500

    Initial commit

---
+++ MonitoreoDeArchivos/View/CustomControls/TextBoxWithPlaceHolder.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileSystemWatcher.View.CustomControls
+{
+    /// <summary>
+    /// Lógica de interacción para TextBoxWithPlaceHolder.xaml
+    /// </summary>
+    public partial class TextBoxWithPlaceHolder : UserControl
+    {
+        public TextBoxWithPlaceHolder()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MonitoreoDeArchivos/View/LoginUI.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FileSystemWatcher.View
+{
+    /// <summary>
+    /// Lógica de interacción para LoginUI.xaml
+    /// </summary>
+    public partial class LoginUI : Window
+    {
+        public LoginUI()
+        {
+            this.InitializeComponent();
+            this.mainframe.Navigate(new Uri("View/Pages/LoginPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
